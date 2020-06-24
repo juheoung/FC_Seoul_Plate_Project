c812f4e4b999e1fcdad89396ecc00952c036ee77
hsw0905 <hsw0905@gmail.com>
2020-06-24 14:59:05 +0900

    feat:review-testcode(list,retrieve)

--- Seoul_Plate/review/serializers.py
+++ Seoul_Plate/review/serializers.py
@@ -6,5 +6,10 @@ from review.models import Review
 class ReviewSerializer(ModelSerializer):
     class Meta:
         model = Review
-        # fields = ['id', 'owner_rest', 'owner_user','review_text' ,'review_image', 'taste_value', 'updated_at']
-        fields = ['id', 'owner_rest', 'review_text' ,'review_image', 'taste_value', 'updated_at']
+        fields = ('id',
+                  'owner_rest',
+                  'owner_user',
+                  'review_text',
+                  'review_image',
+                  'taste_value',
+                  'updated_at',)
--- Seoul_Plate/review/tests.py
+++ Seoul_Plate/review/tests.py
@@ -11,19 +11,27 @@ from review.models import Review
 
 class UserTestCase(APITestCase):
     def setUp(self) -> None:
-        self.test_review = baker.make(Review,
-                                      review_image='https://rforcats.net/assets/img/programmer.png',
-                                      taste_value='GOOD',
-                                      review_text='taste testing')
+        self.test_reviews = baker.make('review.Review', _quantity=3)
 
     def test_should_list_review(self):
         # self.client.force_authenticate(user=self.testAccount)
         response = self.client.get('/api/reviews/')
-
-        print(self.test_review.id)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        review_response = Munch(response.data)
-        self.assertTrue(review_response.id)
-        self.assertEqual(review_response.review_text, self.test_review.review_text)
-        self.assertEqual(review_response.revies_image, self.test_review.image)
+
+        for response_review, origin_review in zip(response.data, self.test_reviews):
+            self.assertEqual(response_review['id'], origin_review.id)
+            self.assertEqual(response_review['review_text'], origin_review.review_text)
+            # self.assertEqual(response_review['review_image'], origin_review.review_image)
+
+    def test_should_get_review(self):
+        test_review = self.test_reviews[0]
+        self.client.force_authenticate(user=test_review)
+        response = self.client.get(f'/api/reviews/{test_review.id}')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['id'], test_review.id)
+        self.assertEqual(response.data['review_text'], test_review.review_text)
+        # self.assertEqual(response.data['review_image'], test_review.review_image)
+
+    def test_should_create_review(self):
+        data = {""}
         self.fail()
