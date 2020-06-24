4f22feaab6bb6dc2c52e19f198a8cb3f3a4836d4
juheoung <juheoung0201@gmail.com>
2020-06-24 10:40:33 +0900

    update blog

--- Seoul_Plate/blogs/models.py
+++ Seoul_Plate/blogs/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
+
 class Blog(models.Model):
     post_title = models.CharField(max_length=200)
     post_contents = models.TextField()
     post_image = models.ImageField(upload_to='blogs_images')
-    post_owner = models.ForeignKey('', on_delete= models.CASCADE)
+    # post_owner = models.ForeignKey('', on_delete=models.CASCADE)
     post_date = models.DateTimeField(auto_now_add=True)
-
-
