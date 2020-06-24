3773d056d6794a2679c7c3e8d930f1ed15fb7023
withHappy <hap2.dev@gmail.com>
2020-06-24 12:28:18 +0900

    feat : update blogs crud

--- Seoul_Plate/Seoul_Plate/urls.py
+++ Seoul_Plate/Seoul_Plate/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,5 +23,4 @@ urlpatterns = [
     path('api/reviews/', include('review.urls')),
     path('api/blogs/', include('blogs.urls')),
     path('api/restaurant/', include('restaurant.urls')),
-
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
