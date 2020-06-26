93c0b0ef75cc5d52b86a1197de804487bd166e72
withHappy <hap2.dev@gmail.com>
2020-06-26 11:07:48 +0900

    refactor : add blogs, bookmark comments

--- Seoul_Plate/blogs/models.py
+++ Seoul_Plate/blogs/models.py
@@ -3,9 +3,14 @@ from django.db import models
 
 
 class Blog(models.Model):
+    # 포스트 제목
     post_title = models.CharField(null=False, max_length=200)
+    #  포스트 내용
     post_contents = models.TextField(null=False)
+    #  포스트 이미지
     post_image = models.ImageField(upload_to='blog_image', null=True)
+    #  포스트 작성 유저
     post_owner = models.ForeignKey(User, default='', null=False, on_delete=models.CASCADE)
+    #  포스트 생성 날짜
     post_date = models.DateTimeField(auto_now_add=True)
 
--- Seoul_Plate/blogs/views.py
+++ Seoul_Plate/blogs/views.py
@@ -13,4 +13,5 @@ class BlogViewSet(ModelViewSet):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
+        """포스트 생성시 로그인 유저 저장"""
         serializer.save(post_owner=self.request.user)
