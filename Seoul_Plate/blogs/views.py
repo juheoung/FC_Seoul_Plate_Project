7cc3bb1d06c303a2fc2d77fa54d650d6168ad21b
withHappy <hap2.dev@gmail.com>
2020-06-26 13:02:58 +0900

    feat : add blogs pagination / restaurant searchfilter

--- Seoul_Plate/blogs/views.py
+++ Seoul_Plate/blogs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import permissions
+from rest_framework.pagination import CursorPagination
 from rest_framework.viewsets import ModelViewSet
 
 from blogs.models import Blog
@@ -7,10 +8,15 @@ from blogs.permissions import IsOwnerOrReadOnly
 from blogs.serializers import BlogSerializer
 
 
+class BlogPagination(CursorPagination):
+    ordering = '-id'
+
+
 class BlogViewSet(ModelViewSet):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    pagination_class = BlogPagination
 
     def perform_create(self, serializer):
         """포스트 생성시 로그인 유저 저장"""
